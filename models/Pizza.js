0bffe189513abe01aedc325898986c288bc7c0ca
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-10-08 21:26:02 -0400

    comments functionality added

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,7 @@
 // Schema constructor and model function 
 const { Schema, model} = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -10,13 +12,35 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
+    toppings: [],
+
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref:'Comment'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+// get total count of comments and replies on retrieval
+
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 //create the Pizza model using the PizzaSchema
